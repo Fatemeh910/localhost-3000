9edc98460d1ee44d24d023ab8a50bce66d4e8889
fatemeh <fatemehkhoshnod91@gmail.com>
2023-01-29 01:55:13 +0330

    Turncated projact

---
+++ src/component/header/header.js
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <div className="bg-blue-200 ">
+            <div className="bg-blue-700 p-12 w-11">
+
+            </div>
+        </div>
+    )
+}
